e1b3bd1cc7beed188c5015680fffa5c72004ac34
Ramesh <chand7272@gmail.com>
2021-05-04 15:55:57 +0530

    Commit message from assign develper !!

---
+++ src/test/developer_data.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class developer_data {
+	
+	@Test
+	public void java_developer(){
+		System.out.println("Java developer");
+	}
+
+	@Test
+	public void php_developer(){
+		System.out.println("Php developer");
+	}
+}
